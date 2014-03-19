ccbe2252e2eac3ae9864763fea30b534e07c42cf
stem mobile <stemmobile@stem-mobile3.local>
2014-03-19 16:00:43 -0400

    Initial Commit

---
+++ WakeTV/WTVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTVAppDelegate.h
+//  WakeTV
+//
+//  Created by stem mobile on 3/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WakeTV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WakeTV
+//
+//  Created by stem mobile on 3/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WTVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WTVAppDelegate class]));
+    }
+}
